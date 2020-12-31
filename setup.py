cc013e1bbf301b2ccd6dee75b74dac4a34f84e29
“vbintx <fetaxyu@gmail.com>
2020-12-31 20:20:16 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,19 +1,34 @@
-# -*- coding: utf-8 -*-
+import os
 from setuptools import setup, find_packages
 
 
 exec(open('iching/version.py').read())
 
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+
 setup(
     name = 'iching',
-    description = 'Any vital question you may have, ask it here.',
     packages = find_packages(exclude=['test']),
     version = __version__,
     author = 'Noa Swartz',
     author_email="fetaxyu@gmail.com",
     url = "https://github.com/vbintx/iching",
     license="Apache License 2.0",
+    description = 'Any vital question you may have, ask it here.',
+    long_description = README,
+    keywords = 'I-Ching',
     python_requires = '>=3.6',
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
 )
 
 # run with: python setup.py bdist_wheel
